a4a0854d974f7f840508f5af29a62eaf70703caa
kiwih <hammondpearce@outlook.com>
2024-09-24 09:30:11 +1000

    Remove unneeded dependency

--- rteparser/pparse.go
+++ rteparser/pparse.go
@@ -2,10 +2,9 @@ package rteparser
 
 import (
 	"github.com/PRETgroup/easy-rte/rtedef"
-	"github.com/PRETgroup/goFB/iec61499"
 )
 
-//pParse is the containing struct for the parsing code
+// pParse is the containing struct for the parsing code
 type pParse struct {
 	funcs []rtedef.EnforcedFunction
 
@@ -16,15 +15,20 @@ type pParse struct {
 	currentFile string
 }
 
-//getCurrentDebugInfo returns the debug info for the last popped item
-func (t *pParse) getCurrentDebugInfo() iec61499.DebugInfo {
-	return iec61499.DebugInfo{
+type DebugInfo struct {
+	SourceLine int
+	SourceFile string
+}
+
+// getCurrentDebugInfo returns the debug info for the last popped item
+func (t *pParse) getCurrentDebugInfo() DebugInfo {
+	return DebugInfo{
 		SourceLine: t.currentLine,
 		SourceFile: t.currentFile,
 	}
 }
 
-//isNameUnused will check all registered functions to see if a name can be used (as they need to be unique)
+// isNameUnused will check all registered functions to see if a name can be used (as they need to be unique)
 func (t *pParse) isNameUnused(name string) bool {
 	for i := 0; i < len(t.funcs); i++ {
 		if t.funcs[i].Name == name {
@@ -34,7 +38,7 @@ func (t *pParse) isNameUnused(name string) bool {
 	return true
 }
 
-//pop gets the current element of the pParse internal items slice
+// pop gets the current element of the pParse internal items slice
 // and increments the index
 func (t *pParse) pop() string {
 	if t.done() {
@@ -50,7 +54,7 @@ func (t *pParse) pop() string {
 	return s
 }
 
-//peek gets the current element of the pParse internal items slice (or the next non-newline character)
+// peek gets the current element of the pParse internal items slice (or the next non-newline character)
 // without incrementing the index
 func (t *pParse) peek() string {
 	if t.done() {
@@ -64,12 +68,12 @@ func (t *pParse) peek() string {
 	return ""
 }
 
-//done checks to see if the pParse is completed (i.e. nothing left to parse)
+// done checks to see if the pParse is completed (i.e. nothing left to parse)
 func (t *pParse) done() bool {
 	return t.itemIndex >= len(t.items)
 }
 
-//getIndexFromName will search the pParse slice of FBs for one that matches
+// getIndexFromName will search the pParse slice of FBs for one that matches
 // the provided name and return the index if found
 func (t *pParse) getIndexFromName(name string) int {
 	for i := 0; i < len(t.funcs); i++ {
